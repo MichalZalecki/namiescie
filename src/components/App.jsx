04ff6bc9dd5b6ae866a793a8f7d9c419f6f2d099
Michal Zalecki <michal@michalzalecki.com>
2016-04-09 11:12:49 +0200

    Display map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,83 @@
 import React from "react";
+import { GoogleMapLoader, GoogleMap, Marker } from "react-google-maps";
 import cls   from "./App.css";
+import uuid from "node-uuid";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      text: "Hello World!",
+      me: {
+        id: uuid.v4(),
+        sex: "male",
+        position: {
+          lat: 51.085845,
+          lng: 17.009627,
+        }
+      },
+      others: [
+        {
+          id: uuid.v4(),
+          sex: "male",
+          position: {
+            lat: 51.084812,
+            lng: 17.013667,
+          }
+        },
+        {
+          id: uuid.v4(),
+          sex: "male",
+          position: {
+            lat: 51.087775,
+            lng: 17.013924,
+          }
+        },
+        {
+          id: uuid.v4(),
+          sex: "male",
+          position: {
+            lat: 51.087406,
+            lng: 17.007773,
+          }
+        },
+      ]
     };
+  }
+
+  pingUser(person) {
+    //
   }
 
   render() {
     return (
-      <div className={ cls.app }>
-        <h1 className={ cls.app__title }>{ this.state.text }</h1>
-      </div>
+      <GoogleMapLoader
+        containerElement={
+          <div
+            {...this.props}
+            style={{
+              height: `100%`,
+            }}
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            defaultZoom={ 16 }
+            defaultCenter={ this.state.me.position }
+          >
+            <Marker
+              position={ this.state.me.position }
+            />
+            {
+              this.state.others.map(attrs => <Marker
+                key={ attrs.id }
+                position={ attrs.position }
+                onClick={ () => this.pingUser(attrs) }
+              />)
+            }
+          </GoogleMap>
+        }
+      />
     );
   }
 }
